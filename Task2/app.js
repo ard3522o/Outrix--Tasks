90758664e3bbe5cd61db7a20ba0e35cb18db64cb
Abhay Pratap Singh <145631801+ard3522o@users.noreply.github.com>
2025-06-02 15:00:04 +0530

    2

---
+++ Task2/app.js
@@ -0,0 +1,11 @@
+function addTask() {
+    const inp = document.getElementById("task");
+    if(inp.value === ""){
+        return;
+    }else{
+    console.log(inp.value);
+   
+}
+
+}
+// addTask();
